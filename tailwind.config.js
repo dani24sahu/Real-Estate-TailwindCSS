64ff2628e05a3c6ed8a9b2c2891903eb0b076f81
Dani <daniramsahu2001@gmail.com>
2023-06-11 16:43:13 +0530

    Created custum color shades

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,24 @@ export default {
       // Creating custom theme
       // Overwriting existing font-sizes
       fontSize: {
-        sm: "0.7rem",  //12px
-        base: "0.875rem",  //14px
-        md: "1rem",  //16px
-        lg: "1.25rem",  //20px
-        xl: "1.5rem",  //24px
-        "2xl": "1.75rem",  //28px
-        "3xl": "2.25rem",  //36px
+        sm: "0.7rem", //12px
+        base: "0.875rem", //14px
+        md: "1rem", //16px
+        lg: "1.25rem", //20px
+        xl: "1.5rem", //24px
+        "2xl": "1.75rem", //28px
+        "3xl": "2.25rem", //36px
+      },
+      // Creating custom color shades
+      colors: {
+        black:{
+          400:"#ffffff",
+          500: "E5E5E5",
+          600:"#C4C4C4",
+          700:"#242527",
+          800:"#282828",
+          900:"#000000",
+        }
       },
     },
   },
