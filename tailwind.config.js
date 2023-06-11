3c6f239479378e70b48f786c0afe9febd3c814f8
Dani <daniramsahu2001@gmail.com>
2023-06-11 14:30:19 +0530

    Created custom font sizes under tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      // Creating custom theme
+      // Overwriting existing font-sizes
+      fontSize: {
+        sm: "0.7rem",  //12px
+        base: "0,875rem",  //14px
+        md: "1rem",  //16px
+        lg: "1.25rem",  //20px
+        xl: "1.5rem",  //24px
+        "2xl": "1.75rem",  //28px
+        "3xl": "2.25rem",  //36px
+      },
+    },
   },
   plugins: [],
 };
